c76e14750923f2c10c8c10994a3ce343a3919b99
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-31 22:20:44 +0530

    Create App.jsx

---
+++ Day 7: State Management/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./Counter";
+import LiveText from "./LiveText";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <LiveText />
+    </div>
+  );
+}
+
+export default App;
